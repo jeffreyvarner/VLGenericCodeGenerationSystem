14331171b360bd8be9f590fac4200c7764e25b40
jeffreyvarner <jeff@curveos.com>
2014-02-04 16:37:39 -0500

    Initial Commit

---
+++ VLGenericCodeGenerationSystem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VLGenericCodeGenerationSystem
+//
+//  Created by Jeffrey Varner on 2/4/14.
+//  Copyright (c) 2014 Varnerlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
