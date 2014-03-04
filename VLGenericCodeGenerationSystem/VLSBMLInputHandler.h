8b609c91c024a376001292af3af77601c5e904e5
jeffreyvarner <jeff@curveos.com>
2014-03-04 08:01:49 -0500

    Copyright and mapping file

---
+++ VLGenericCodeGenerationSystem/VLSBMLInputHandler.h
@@ -0,0 +1,13 @@
+//
+//  VLSBMLInputHandler.h
+//  VLGenericCodeGenerationSystem
+//
+//  Created by Jeffrey Varner on 3/4/14.
+//  Copyright (c) 2014 Varnerlab. All rights reserved.
+//
+
+#import "VLAbstractInputHandler.h"
+
+@interface VLSBMLInputHandler : VLAbstractInputHandler
+
+@end
