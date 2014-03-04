c47b6a7707cedea9c4debbddd6b1903f6695c299
jeffreyvarner <jeff@curveos.com>
2014-03-04 15:32:21 -0500

    some changes

--- conf/model/src/Kinetics.m
+++
@@ -1,7 +0,0 @@
-function [rV]= Kinetics(t,x,kV,DF)
-% Build the kinetic expression for each model rate -- 
-NUMBER_OF_RATES = DF.NUMBER_OF_RATES;
-rV = zeros(NUMBER_OF_RATES,1);
-
-% Alias the parameters - 
-return;
