b11e1a770a8f617287c07a33f15cdb18195283b2
jeffreyvarner <jeff@curveos.com>
2014-03-06 07:28:41 -0500

    stm SBML

--- VLGenericCodeGenerationSystem/VLSharedUtilitiesOutputHandler.m
+++ VLGenericCodeGenerationSystem/VLSharedUtilitiesOutputHandler.m
@@ -23,46 +23,102 @@ static const NSString *kStoichiometricCoefficient = @"STOICHIOMETRIC_COEFFICIENT
     NSMutableString *buffer = [[NSMutableString alloc] init];
     
     // Get our trees from the dictionary -
-    __unused NSXMLDocument *transformation_tree = [options objectForKey:@"TRANSFORMATION_TREE"];
-    __unused NSXMLElement *transformation = [options objectForKey:@"TRANSFORMATION_XML_ELEMENT"];
-    NSXMLDocument *input_tree = (NSXMLDocument *)[options objectForKey:@"INPUT_DATA_TREE"];
+    __unused NSXMLDocument *transformation_tree = [options objectForKey:kXMLTransformationTree];
+    __unused NSXMLElement *transformation = [options objectForKey:kXMLTransformationElement];
+    NSXMLDocument *input_tree = (NSXMLDocument *)[options objectForKey:kXMLModelInputTree];
     
-    // need to build the stm array -
-    // ok, so we have to write *each* balance equation out by hand (not the compact form) -
-    NSString *xpathString = @"//species";
-    NSArray *list_of_species = [input_tree nodesForXPath:xpathString error:nil];
-    for (NSXMLElement *species_node in list_of_species)
+    // What is my model encoding?
+    NSString *model_source_encoding = [[[input_tree nodesForXPath:@".//model/@source_encoding" error:nil] lastObject] stringValue];
+    
+    if ([model_source_encoding isEqualToString:kSourceEncodingVFF] == YES)
     {
-        // get the symbol -
-        NSString *raw_symbol = [[species_node attributeForName:@"symbol"] stringValue];
-        if ([raw_symbol isEqualToString:@"[]"] == NO)
+        // need to build the stm array -
+        // ok, so we have to write *each* balance equation out by hand (not the compact form) -
+        NSString *xpathString = @"//species";
+        NSArray *list_of_species = [input_tree nodesForXPath:xpathString error:nil];
+        for (NSXMLElement *species_node in list_of_species)
         {
-            // get my list of interactions -
-            NSArray *reaction_array = [input_tree nodesForXPath:@".//interaction" error:nil];
-            for (NSXMLElement *reaction_node in reaction_array)
+            // get the symbol -
+            NSString *raw_symbol = [[species_node attributeForName:@"symbol"] stringValue];
+            if ([raw_symbol isEqualToString:@"[]"] == NO)
             {
-                // Get the reactant and product strings -
-                NSString *reactant_string = [[[reaction_node nodesForXPath:@"./interaction_input/@reaction_string" error:nil] lastObject] stringValue];
-                NSString *product_string = [[[reaction_node nodesForXPath:@"./interaction_output/@reaction_string" error:nil] lastObject] stringValue];
-
-                // is this species a involved in this reaction as a reactant?
-                NSDictionary *reactant_dictionary = [self evaluateSpeciesSymbol:raw_symbol inReactionString:reactant_string];
-                NSDictionary *product_dictionary = [self evaluateSpeciesSymbol:raw_symbol inProductString:product_string];
+                // get my list of interactions -
+                NSArray *reaction_array = [input_tree nodesForXPath:@".//interaction" error:nil];
+                for (NSXMLElement *reaction_node in reaction_array)
+                {
+                    // Get the reactant and product strings -
+                    NSString *reactant_string = [[[reaction_node nodesForXPath:@"./interaction_input/@reaction_string" error:nil] lastObject] stringValue];
+                    NSString *product_string = [[[reaction_node nodesForXPath:@"./interaction_output/@reaction_string" error:nil] lastObject] stringValue];
+                    
+                    // is this species a involved in this reaction as a reactant?
+                    NSDictionary *reactant_dictionary = [self evaluateSpeciesSymbol:raw_symbol inReactionString:reactant_string];
+                    NSDictionary *product_dictionary = [self evaluateSpeciesSymbol:raw_symbol inProductString:product_string];
+                    
+                    // if *both* the reactant and product dictionaries are nil, then *not* involved in this reaction
+                    if (reactant_dictionary == nil && product_dictionary == nil)
+                    {
+                        [buffer appendString:@" 0.0 "];
+                    }
+                    else if (reactant_dictionary !=nil && product_dictionary == nil)
+                    {
+                        NSString *stcoeff = [reactant_dictionary objectForKey:kStoichiometricCoefficient];
+                        [buffer appendFormat:@" %@ ",stcoeff];
+                    }
+                    else if (reactant_dictionary == nil && product_dictionary != nil)
+                    {
+                        NSString *stcoeff = [product_dictionary objectForKey:kStoichiometricCoefficient];
+                        [buffer appendFormat:@" %@ ",stcoeff];
+                    }
+                }
                 
-                // if *both* the reactant and product dictionaries are nil, then *not* involved in this reaction
-                if (reactant_dictionary == nil && product_dictionary == nil)
+                // next row -
+                [buffer appendString:@"\n"];
+            }
+        }
+    }
+    else if ([model_source_encoding isEqualToString:kSourceEncodingSBML] == YES)
+    {
+        NSString *xpathString = @".//species";
+        NSArray *list_of_species = [input_tree nodesForXPath:xpathString error:nil];
+        for (NSXMLElement *species_node in list_of_species)
+        {
+            // get the symbol -
+            NSString *raw_symbol = [[species_node attributeForName:@"id"] stringValue];
+            if ([raw_symbol isEqualToString:@"[]"] == NO)
+            {
+                // Get the reactant products -
+                NSArray *reaction_array = [input_tree nodesForXPath:@".//reaction" error:nil];
+                for (NSXMLElement *reaction_node in reaction_array)
                 {
-                    [buffer appendString:@" 0.0 "];
-                }
-                else if (reactant_dictionary !=nil && product_dictionary == nil)
-                {
-                    NSString *stcoeff = [reactant_dictionary objectForKey:kStoichiometricCoefficient];
-                    [buffer appendFormat:@" %@ ",stcoeff];
-                }
-                else if (reactant_dictionary == nil && product_dictionary != nil)
-                {
-                    NSString *stcoeff = [product_dictionary objectForKey:kStoichiometricCoefficient];
-                    [buffer appendFormat:@" %@ ",stcoeff];
+                    // for this reaction - what are the reactants?
+                    NSString *xpath_reactant = [NSString stringWithFormat:@"./listOfReactants/speciesReference[@species='%@']/@stoichiometry",raw_symbol];
+                    NSString *stcoeff_reactant = [[[reaction_node nodesForXPath:xpath_reactant error:nil] lastObject] stringValue];
+                    
+                    // for this reaction - what are the products?
+                    NSString *xpath_product = [NSString stringWithFormat:@"./listOfProducts/speciesReference[@species='%@']/@stoichiometry",raw_symbol];
+                    NSString *stcoeff_product = [[[reaction_node nodesForXPath:xpath_product error:nil] lastObject] stringValue];
+
+                    // ok - if we have *no* stcoeff, then 0.0
+                    if (stcoeff_reactant == nil &&
+                        stcoeff_product == nil)
+                    {
+                        [buffer appendString:@" 0.0 "];
+                    }
+                    else if (stcoeff_reactant != nil &&
+                             stcoeff_product == nil)
+                    {
+                        [buffer appendFormat:@" %@ ",stcoeff_reactant];
+                    }
+                    else if (stcoeff_reactant == nil &&
+                             stcoeff_product != nil)
+                    {
+                        [buffer appendFormat:@" %@ ",stcoeff_product];
+                    }
+                    else if (stcoeff_reactant == nil &&
+                             stcoeff_product != nil)
+                    {
+                        // ok - would this ever happen?
+                    }
                 }
             }
             
@@ -70,7 +126,6 @@ static const NSString *kStoichiometricCoefficient = @"STOICHIOMETRIC_COEFFICIENT
             [buffer appendString:@"\n"];
         }
     }
-
     
     // write -
     [self writeCodeGenerationOutput:buffer toFileWithOptions:options];
